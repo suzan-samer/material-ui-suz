f5265566b144e1f7795070394ebb7c23726fdaf4
suzan-samer <78270717+suzan-samer@users.noreply.github.com>
2023-03-20 21:06:37 +0200

    Header

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-
+import React, { useState, useContext } from "react";
+import { Outlet } from "react-router-dom";
+import { AppBar, Avatar, Link, Typography, Toolbar } from "@mui/material";
 const Root = () => {
   return (
     <>
-    <header>My Header</header>
-
-    <Outlet />
+      <AppBar position="static" sx={{ background: "#000000" }}>
+        <Toolbar>
+          <Link
+            variant="h6"
+            color="inherit"
+            underline="none"
+            sx={{ flexGrow: 1, "&:hover": { color: "pink", fontSize: "22px" } }}
+            href="/"
+          >
+            CLUB MONAKO
+          </Link>
+          <Typography variant="body1" color="inherit">
+            SUZAN SAMER
+          </Typography>
+          <Avatar
+            sx={{ marginLeft: "12px", bgcolor: "" }}
+            alt="Suzan Samer"
+            src="./images/suz.jpg"
+          />
+        </Toolbar>
+      </AppBar>
+      <Outlet />
     </>
-
   );
-}
+};
 
 export default Root;
